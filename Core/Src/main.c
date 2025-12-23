9f19f8d6b76eb0057e6652dd6413a1664de5c37b
tts <tangts@tof3d.com>
2025-12-23 21:04:43 +0800

    CHANGED: 修改初始化。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -176,8 +176,8 @@ int main(void)
   // 初始化lvgl demo
   // lv_demo_widgets();
   setup_ui(&guider_ui); // 初始化ui
-  init_keyboard(&guider_ui); // 初始化键盘
-  setup_scr_screen(&guider_ui); // 设置屏幕
+  // init_keyboard(&guider_ui); // 初始化键盘
+  // setup_scr_screen(&guider_ui); // 设置屏幕
   custom_init(&guider_ui);
   // LCD_Init();
   // POINT_COLOR=RED;
@@ -258,7 +258,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 576000;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
